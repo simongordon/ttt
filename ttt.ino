2a7c13ad54b7d7261b3b5764af1c86e73e912e2d
Simon Gordon <gordonsimon@hotmail.com>
2018-01-03 18:06:15 +1000

    Stub program

---
+++ ttt.ino
@@ -0,0 +1,19 @@
+#include <Arduboy.h>
+
+Arduboy arduboy;
+
+typedef char SQUARE_VAL;
+
+#define NUM_SQUARES 9;
+
+int selector = 0;
+
+void setup()
+{
+  arduboy.begin();
+  arduboy.clear();
+}
+
+void loop()
+{
+}
