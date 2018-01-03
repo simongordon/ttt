3c6b4b6013f32ad166e5d11e65c149f5e9cddeec
Simon Gordon <gordonsimon@hotmail.com>
2018-01-03 18:53:00 +1000

    Allow selector to be moved

--- src/ttt.ino
+++ src/ttt.ino
@@ -12,18 +12,32 @@ typedef char SquareVal;
 
 #define THINGO 20
 
-SquareVal squares[NUM_SQUARES];
+SquareVal squares[SIDE_LENGTH][SIDE_LENGTH];
 
-int selector = 0;
+int selX = 0;
+int selY = 0;
+
+SquareVal player;
+
+void clearBoard()
+{
+  for (int col = 0; col < SIDE_LENGTH; col++)
+  {
+    for (int row = 0; row < SIDE_LENGTH; row++)
+    {
+      squares[col][row] = EMPTY;
+    }
+  }
+}
 
 void setup()
 {
   arduboy.begin();
   arduboy.setFrameRate(15);
-  for (int i = 0; i < NUM_SQUARES; i++)
-  {
-    squares[i] = EMPTY;
-  }
+
+  clearBoard();
+
+  player = CROSS;
 }
 
 void displayBoard()
@@ -38,19 +52,42 @@ void displayBoard()
       int y = row * THINGO;
       arduboy.setCursor(x, y);
       int pos = (SIDE_LENGTH * col) + row;
-      if (pos == selector)
+      if (col == selX && row == selY)
       {
         arduboy.drawRect(x, y, THINGO, THINGO, WHITE);
       }
-      arduboy.print(squares[pos]);
+      arduboy.print(squares[col][row]);
     }
   }
   arduboy.display();
+}
+
+void changeTurn()
+{
+  player = player == CROSS ? NOUGHT : CROSS;
 }
 
 void loop()
 {
   if (!(arduboy.nextFrame()))
     return;
+
+  if (arduboy.pressed(LEFT_BUTTON))
+  {
+    selX = selX - 1;
+  }
+  if (arduboy.pressed(RIGHT_BUTTON))
+  {
+    selX = selX + 1;
+  }
+  if (arduboy.pressed(UP_BUTTON))
+  {
+    selY = selY - 1;
+  }
+  if (arduboy.pressed(DOWN_BUTTON))
+  {
+    selY = selY + 1;
+  }
+
   displayBoard();
 }
